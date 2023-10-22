54f0f28ab67e2062848f9ead3b1102ebd7bf744a
Aaliyah Reid <aaliyahreid12345@gmail.com>
2023-10-22 16:48:19 -0400

    Now handles event when square is clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", function (){
     const gameBoard = document.querySelector('#board');
     const cells = ["","","","","","","","","" ];
+    let currPlayer = 'X'; 
 
 
 function createBoard(){
     cells.forEach((_cell,index) => {
         const squareCell = document.createElement('div');
         squareCell.classList.add('square');
+        squareCell.id = index;
+        squareCell.addEventListener('click' , () => addPlayerSymbol(squareCell, index));
         gameBoard.append(squareCell);
     });
 }
 
 
-createBoard()
+function addPlayerSymbol(squareCell, index){
+    if (cells[index] ==="") {
+        cells[index] = currPlayer;
+        squareCell.classList.add(currPlayer);
+        squareCell.textContent = currPlayer;
+        currPlayer = (currPlayer === 'X') ? 'O' : 'X';
+            
+ }
+}
+
+
+
+createBoard();
 
 });
